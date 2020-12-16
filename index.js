33d3cb265c8e6f82f04386993da862aa307042e0
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-16 07:02:21 -0300

    feat:[wip]cluster configurated

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ require('./redis');
 const express = require('express');
 const server = express();
 const routes = require('./routes');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length
+
 
 server.use(express.json());
 server.use(routes);
+ 
+if(cluster.isMaster) {
+    for(let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+} else {
 
-server.listen(3000);
+    server.listen(3000);
+}
