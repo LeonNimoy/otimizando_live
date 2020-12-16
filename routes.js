e892f5375be8a4f459741310f1d8e1594ad32e28
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-16 07:01:32 -0300

    feat:cashing on routes grades created

--- routes.js
+++ routes.js
@@ -1,9 +1,22 @@
 const Router = require('express').Router;
 const routes = new Router();
 const gradesSchema = require('./schema');
+const cache = require('./redis');
 
 routes.get('/grades', async (req, res) =>{
-    const grades = gradesSchema.find().limit(100);
+    const grades = await gradesSchema.find().limit(100);
+    res.json({data:grades})
+});
+
+routes.get('/grades/:id', async (req, res) =>{
+    const {id} = req.params;
+    const exists = await cache.get(`grades:${id}`);
+    if(exists) {
+        res.json({data: JSON.parse(exists)})
+    }
+    const grades = await gradesSchema.findOne({_id: id});
+    cache.set(`grades:${id}`, JSON.stringify(grades))
+    res.json({data:grades})
 });
 
 module.exports = routes;
