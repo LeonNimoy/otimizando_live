fa9d78fff78a84982104b47951d68553da034f7d
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-22 12:02:41 -0300

    feat:cluster implemented

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ if(cluster.isMaster) {
     for(let i = 0; i < numCPUs; i++) {
         cluster.fork()
     }
+    cluster.on('exit', (worker, code, signal)=>{
+        console.log(`worker ${worker.process.pid} died`);
+    })
 } else {
-
     server.listen(3000);
+    console.log(`Worker ${process.id} started`);
 }
